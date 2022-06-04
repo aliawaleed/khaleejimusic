f6fbf4efb3756a343f5e38b1624e807d67a0e9f9
Umair Shah <umair.shah@live.com>
2022-06-04 15:31:45 +0400

    Changes to Louvre section

---
+++ louvre/script.js
@@ -0,0 +1,22 @@
+let overlay;
+
+window.addEventListener("load", () => {
+    overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+})
+
+function goHome(){
+    window.location = '/index.html';
+}
+
+function display(piece){
+    overlay.style.display = "block";
+    let image = document.getElementById("art-image");
+    image.className = piece;
+    image.src = "/louvre/images/" + piece + ".jpg";
+    console.log(piece);
+}
+
+function back(){
+    overlay.style.display = "none";
+}
