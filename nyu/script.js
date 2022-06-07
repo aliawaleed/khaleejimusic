700b1b7287ca0b2b70d1a6ef5e3b718b1074b02c
aliawaleed <57350259+aliawaleed@users.noreply.github.com>
2022-06-07 18:10:20 +0400

    changed manarat map, worked on nyu

--- nyu/script.js
+++ nyu/script.js
@@ -1,8 +1,10 @@
 let slideIndex = 1;
-let paths = ['/nyu/images/vid1.mp4', '/nyu/images/vid2.mp4', '/nyu/images/vid3.mp4', '/nyu/images/vid4.mp4', '/nyu/images/vid5.mp4', '/nyu/images/vid6.mp4', '/nyu/images/vid7.mp4', '/nyu/images/vid8.mp4'];
+let paths = ['/nyu/images/vid1.mp4', '/nyu/images/vid2.mp4', '/nyu/images/vid3.mp4', '/nyu/images/vid4.mp4', '/nyu/images/vid5.mp4', '/nyu/images/vid6.mp4', '/nyu/images/vid7.mp4', '/nyu/images/vid8.mp4', '/nyu/images/vid9.mp4', '/nyu/images/vid10.mp4', '/nyu/images/vid11.mp4', '/nyu/images/vid12.mp4', '/nyu/images/vid13.mp4', '/nyu/images/vid14.mp4', '/nyu/images/vid15.mp4', '/nyu/images/vid16.mp4', '/nyu/images/vid17.mp4', '/nyu/images/vid18.mp4'];
 
 
 window.addEventListener("load", () => {
+    let videos = document.getElementsByClassName('videos')[0];
+    videos.style.display = "none";
     let container = document.getElementsByClassName('slideshow-container')[0];
     for (let i = 0; i < paths.length; i++) {
         let createSlide = document.createElement('div');
@@ -13,7 +15,7 @@ window.addEventListener("load", () => {
         let source = document.createElement('source');
         source.src = paths[i];
         source.type = 'video/mp4';
-        video.setAttribute("controls","controls")
+        video.setAttribute("controls", "controls")
         video.appendChild(source);
         createSlide.appendChild(video);
         let text = document.createElement('div');
@@ -21,10 +23,27 @@ window.addEventListener("load", () => {
         text.innerHTML = "add caption here";
         createSlide.appendChild(text);
     }
+
+    let dots = document.getElementsByClassName('dots')[0];
+    console.log(dots)
+    for (let i = 0; i < paths.length; i++) {
+        let newDot = document.createElement('span');
+        newDot.classList.add('dot');
+        newDot.onclick = function () { currentSlide(i + 1) };
+        dots.appendChild(newDot);
+    }
     showSlides(slideIndex);
 })
 
-function plusSlides(n) {
+function viewVideos() {
+    let videos = document.getElementsByClassName('videos')[0];
+    videos.style.display = "block";
+    let main = document.getElementsByClassName('main')[0];
+    main.style.display = "none";
+}
+
+
+function changeSlide(n) {
     showSlides(slideIndex += n);
 }
 
