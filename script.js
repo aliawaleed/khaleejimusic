f5d9d1589a8bcdf415b0548c1e27e514e177b830
aliawaleed <57350259+aliawaleed@users.noreply.github.com>
2022-06-09 22:28:07 +0400

    added about page, updated colors for manarat, worked on design for nyu

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function enterNYU() {
 function enterManarat() {
     window.location = '/manarat/index.html';
 }
+
+function displayAbout() {
+    let description = document.getElementsByClassName("description")[0];
+    let about = document.getElementsByClassName("about")[0];
+    let button = document.getElementsByClassName("about-button")[0];
+    if (button.textContent == "About") {
+        description.style.display = "none";
+        about.style.display = "block";
+        button.textContent = "Home";  
+    }
+    else {
+        description.style.display = "block";
+        about.style.display = "none";
+        button.textContent = "About";  
+    }
+
+}
